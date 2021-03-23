9eaedc5b317a016a0c53d577a69aabe4c9c3e72e
NAVANEETH S <navaneethsathyan0@gmail.com>
2021-03-23 16:24:47 +0530

    Python

---
+++ CO3/graphics/graphics/rectangle.py
@@ -0,0 +1,4 @@
+def area(l,w):
+    return l*w
+def pere(l,w):
+    return 2*l+2*w
---
+++ CO3/graphics/main.py
@@ -0,0 +1,17 @@
+import graphics.circle as ci
+import graphics.rectangle as re
+import graphics.subgraphics.cuboid as cu
+import graphics.subgraphics.sphere as sp
+
+print(ci.area(2))
+print(ci.pere(2))
+
+print(re.area(2,3))
+print(re.pere(2,3))
+
+print(sp.area(2))
+print(sp.pere(2))
+
+print(cu.area(1,1,2))
+print(cu.pere(1,1,2))
+
